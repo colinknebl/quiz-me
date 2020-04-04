e8ab57898e02cf033dba17ef7090987e24b38c53
Colin Knebl <colinknebl@outlook.com>
2020-04-04 17:18:13 -0400

    format Page.tsx

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -4,28 +4,28 @@ import { RouteComponentProps } from 'react-router';
 import ExploreContainer from '../components/ExploreContainer';
 import './Page.css';
 
-const Page: React.FC<RouteComponentProps<{ name: string; }>> = ({ match }) => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot="start">
-            <IonMenuButton />
-          </IonButtons>
-          <IonTitle>{match.params.name}</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+const Page: React.FC<RouteComponentProps<{ name: string }>> = ({ match }) => {
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonButtons slot="start">
+                        <IonMenuButton />
+                    </IonButtons>
+                    <IonTitle>{match.params.name}</IonTitle>
+                </IonToolbar>
+            </IonHeader>
 
-      <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">{ match.params.name }</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name={match.params.name} />
-      </IonContent>
-    </IonPage>
-  );
+            <IonContent>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">{match.params.name}</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <ExploreContainer name={match.params.name} />
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Page;
