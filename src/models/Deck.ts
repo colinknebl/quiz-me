75927177974c1e7f0b6722bc913172e9e7fc0163
Colin Knebl <colinknebl@outlook.com>
2020-04-06 19:44:10 -0400

    add static create method on deck model

--- src/models/Deck.ts
+++ src/models/Deck.ts
@@ -1,4 +1,14 @@
+import { App } from './App';
+import { AppError } from '../utils/AppError';
 import { ICard, Card } from './Card';
+
+interface ICreateDeckAPIResponse {
+    code: number;
+    error: string | null;
+    data: {
+        deckId: string;
+    } | null;
+}
 
 export interface IDeck {
     id: string;
@@ -14,5 +24,20 @@ export class Deck {
         if (cards?.length) {
             this.cards = cards.map((card) => new Card(card.id, card.sideA, card.sideB, card.marked));
         }
+    }
+
+    public static async create(title: string): Promise<Deck> {
+        const req = await fetch(`${App.APIBaseURL}/create-deck`, {
+            method: 'POST',
+            headers: App.getRequestHeaders({ withAuth: true }),
+            body: JSON.stringify({
+                title,
+            }),
+        });
+        const res: ICreateDeckAPIResponse = await req.json();
+        if (res.code !== 200 || res.error || !res.data?.deckId) {
+            throw new AppError(res.error || 'Verification failure');
+        }
+        return new Deck(res.data.deckId, title, true, []);
     }
 }
