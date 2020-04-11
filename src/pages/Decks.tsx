b3ae5c86c92470f1db944db1050b43f72aa9ac5d
Colin Knebl <colinknebl@outlook.com>
2020-04-11 15:39:30 -0400

    add no decks message

--- src/pages/Decks.tsx
+++ src/pages/Decks.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { IonContent, IonPage } from '@ionic/react';
+import { IonContent, IonPage, IonButton } from '@ionic/react';
 import { withRouter, RouteComponentProps } from 'react-router-dom';
 
 import { AppContext } from '../context/App.context';
@@ -7,22 +7,20 @@ import DeckCard from '../components/DeckCard';
 import Header from '../components/Header';
 import { useProtectRoute } from '../hooks/useProtectedRoute';
 import './Decks.css';
+import { Deck } from '../models/Deck';
 
 function Decks(props: RouteComponentProps) {
-    useProtectRoute();
+    const redirect = useProtectRoute();
 
     const ctx = useContext(AppContext);
 
+    if (redirect) return redirect;
     return (
         <IonPage>
             <Header />
             <IonContent>
                 <div className="decks">
-                    <div className="deck-grid">
-                        {ctx.user?.decks.map((deck) => (
-                            <DeckCard key={deck.id} deck={deck} />
-                        ))}
-                    </div>
+                    {ctx?.user?.decks?.length ? <DeckList decks={ctx.user!.decks} /> : <NoDecks {...props} />}
                 </div>
             </IonContent>
         </IonPage>
@@ -30,3 +28,34 @@ function Decks(props: RouteComponentProps) {
 }
 
 export default withRouter(Decks);
+
+function DeckList({ decks }: { decks: Deck[] }) {
+    return (
+        <div className="deck-grid">
+            {decks.map((deck) => (
+                <DeckCard key={deck.id} deck={deck} />
+            ))}
+        </div>
+    );
+}
+
+function NoDecks(props: RouteComponentProps) {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}
+        >
+            <p>No decks available</p>
+            <IonButton
+                onClick={() => {
+                    props.history.push('/p/create-deck');
+                }}
+            >
+                Create Deck
+            </IonButton>
+        </div>
+    );
+}
