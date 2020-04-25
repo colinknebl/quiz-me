dc184f327ddad032822f412ce379a2b9f72758b3
Colin Knebl <colinknebl@outlook.com>
2020-04-25 17:42:11 -0400

    add success check

--- src/models/Card.ts
+++ src/models/Card.ts
@@ -5,41 +5,52 @@ import { AppError } from '../utils/AppError';
 type CreateCardResponse = APIResponse<{ cardId: string }>;
 
 export interface ICard {
-    id: string;
-    sideA: string;
-    sideB: string;
-    marked: boolean;
+	id: string;
+	sideA: string;
+	sideB: string;
+	marked: boolean;
 }
 
 export class Card {
-    constructor(
-        private _deck: Deck,
-        public id: string,
-        public sideA: string,
-        public sideB: string,
-        public isMarked: boolean
-    ) {}
+	constructor(
+		private _deck: Deck,
+		public id: string,
+		public sideA: string,
+		public sideB: string,
+		public isMarked: boolean
+	) {}
 
-    public toggleMarked(): Promise<boolean> {
-        this.isMarked = !this.isMarked;
-        return this._deck.toggleCard(this.id);
-    }
+	public toggleMarked(): Promise<boolean> {
+		this.isMarked = !this.isMarked;
+		return this._deck.toggleCard(this.id);
+	}
 
-    public static async create(deck: Deck, sideA: string, sideB: string): Promise<Card> {
-        const options = App.getRequestOptions({
-            withAuth: true,
-            method: 'POST',
-            body: { sideA, sideB },
-        });
-        const req = await fetch(`${App.APIBaseURL}/decks/${deck.id}/cards`, options);
-        const res: CreateCardResponse = await req.json();
+	public static async create(
+		deck: Deck,
+		sideA: string,
+		sideB: string
+	): Promise<Card> {
+		const options = App.getRequestOptions({
+			withAuth: true,
+			method: 'POST',
+			body: { sideA, sideB },
+		});
+		const req = await fetch(
+			`${App.APIBaseURL}/decks/${deck.id}/cards`,
+			options
+		);
+		const res: CreateCardResponse = await req.json();
 
-        if (res.error) {
-            throw new AppError(res.error);
-        }
+		if (res.error) {
+			throw new AppError(res.error);
+		}
 
-        const card = new Card(deck, res.data.cardId, sideA, sideB, false);
-        deck.addCard(card);
-        return card;
-    }
+		if (res.code !== 200 || res.error) {
+			throw new AppError(res.error || 'Error creating card');
+		}
+
+		const card = new Card(deck, res.data.cardId, sideA, sideB, false);
+		deck.addCard(card);
+		return card;
+	}
 }
